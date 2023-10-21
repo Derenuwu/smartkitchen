c1144b0ef8cd1e32b1173bf20f6de6985c17344f
unknown <Derenmei1030@gmail.com>
2023-10-21 14:37:20 -0400

    remove the whole index directory and pull it out, because it causes error, also adding some untracked files

---
+++ src/main/java/com/sm_kitchen/service/impl/TableServiceImpl.java
@@ -0,0 +1,50 @@
+package com.sm_kitchen.service.impl;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.sm_kitchen.bean.Table;
+import com.sm_kitchen.dao.TableDao;
+import com.sm_kitchen.dao.impl.TableDaoImpl;
+import com.sm_kitchen.exception.DishGetException;
+import com.sm_kitchen.service.TableService;
+import com.sm_kitchen.utils.DBUtil;
+
+public class TableServiceImpl implements TableService {
+
+   private TableDao tableDao = new TableDaoImpl(); // moditly db record obj
+
+   @Override
+   public List<Table> getAllTables() throws Exception {
+      List<Table> result = null;
+      try ( Connection connection = DBUtil.getConnection() ) {
+         connection.setAutoCommit(false);
+         
+         // todo
+         result = tableDao.selectAll();
+
+         connection.commit();
+      } catch (SQLException e) {
+         throw new DishGetException("Dish display error");
+      }
+      return result;
+   }
+
+   @Override
+   public Table getTableinfo(Integer tableid) throws Exception {
+      Table tableinfo = null;
+      try ( Connection connection = DBUtil.getConnection() ) {
+         connection.setAutoCommit(false);
+         
+         // todo
+         tableinfo = tableDao.selectDishByTableID(tableid);
+
+         connection.commit();
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      return tableinfo;
+   }
+   
+}
