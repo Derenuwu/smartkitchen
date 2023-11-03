cc6c7714f55895e5624f219b58d3ca4cc160143a
unknown <Derenmei1030@gmail.com>
2023-11-03 12:59:01 -0400

    A cart class use for storing info about the item

---
+++ src/main/java/com/sm_kitchen/bean/Cart.java
@@ -0,0 +1,30 @@
+package com.sm_kitchen.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Dish> items;
+    private double total;
+
+    public Cart() {
+        this.items = new ArrayList<>();
+        this.total = 0.0;
+    }
+
+    public List<Dish> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Dish> items) {
+        this.items = items;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
