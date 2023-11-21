db523eaedfe2543f20b1a453ae6f6adc96c203ee
unknown <Derenmei1030@gmail.com>
2023-11-20 20:36:42 -0500

    modified function that able to remove items in cart

--- src/main/java/com/sm_kitchen/controller/RemoveFromCartServlet.java
+++ src/main/java/com/sm_kitchen/controller/RemoveFromCartServlet.java
@@ -12,7 +12,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet("/api/remove-from-cart")
@@ -24,16 +27,20 @@ public class RemoveFromCartServlet extends HttpServlet {
             throws ServletException, IOException {
         // Get JSON data from the request
         String requestBody = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+        Map<String, Object> datamap = this.objectMapper.readValue(requestBody, Map.class);
 
-        // Parse the JSON data into a Dish object
-        Dish dish = objectMapper.readValue(requestBody, Dish.class);
+        Dish dish = new Dish(null, String.valueOf(datamap.get("name")),
+                Double.parseDouble(String.valueOf(datamap.get("price"))));
 
-        // Handle the logic of removing from the shopping cart
-        Cart cart = cartService.removeFromCart(dish);
+        String cartItemsJson = objectMapper.writeValueAsString(datamap.get("cartitems"));
+        Dish[] dishesArr = objectMapper.readValue(cartItemsJson, Dish[].class);
+        ArrayList<Dish> dishes = new ArrayList<>(Arrays.asList(dishesArr));
+
+        Cart cart = cartService.removeFromCartA(dishes, dish);
 
         // Construct response object with updated total
         Map<String, Object> jsonResponse = new HashMap<>();
-        jsonResponse.put("cart", cart.getItemsArray());  // 使用新添加的方法获取数组
+        jsonResponse.put("cart", cart.getItemsArray());
         jsonResponse.put("total", cart.getTotal());
 
         // Set the response Content-Type to JSON
@@ -44,8 +51,3 @@ public class RemoveFromCartServlet extends HttpServlet {
         response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
     }
 }
-
-
-
-
-
